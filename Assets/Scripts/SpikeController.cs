d67455c3b2c84015dc0914036b66b13599ff8c5d
HC <51252433+bchou9@users.noreply.github.com>
2022-05-21 19:43:13 -0700

    Implement spikes

---
+++ Assets/Scripts/SpikeController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeController : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var player = collision.gameObject.GetComponent<HeroKnight>();
+        if (player != null)
+        {
+            // var ev = Schedule<PlayerEnemyCollision>();
+            // ev.player = player;
+            // ev.enemy = this;
+            int damage = player.PlayerHurt(100);
+            if(damage > 0)
+            {
+                damage = damage;
+            }
+            else
+            {
+                player.PlayerDeath();
+            }
+            
+        }
+    }
+}
