b358c3b4b0f249632b0024c9a0f116849b361ed9
breliu-dv <breliu@ucdavis.edu>
2022-06-01 15:50:16 -0700

    moved/reworked all acid logic to acidcontroller, switched spike prefabs to spikehorizontal not acidpool

--- Assets/Scripts/AcidPool.cs
+++ Assets/Scripts/AcidPool.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,29 +6,41 @@ using UnityEngine;
 public class AcidPool : MonoBehaviour
 {
     private int damage = 10;
+    private float damageInterval = 2.0f;
+    private float nextDamage = 0.0f;
     private bool enteredAcid = false;
-    private KnightController player;
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        player = collision.gameObject.GetComponent<KnightController>();
-        //Debug.Log(player);
-        if (player != null)
-        {
-            enteredAcid = true;
-            player.DoDamage(damage);
-        }
-    }
+    [SerializeField]
+    private GameObject player;
 
-    public bool GetAcidStatus()
-    {
-        return enteredAcid;
-    }
+    private BoxCollider2D collider;
 
+    void Awake() {
+        this.collider = this.GetComponent<BoxCollider2D>();
+    }
     void Update()
     {
-        if (enteredAcid)
+        if (this.collider.IsTouching(player.GetComponent<CapsuleCollider2D>()))
         {
-            player.GetComponent<Animator>().SetBool("Grounded", true);
+            Action<int> DamageKnight = player.GetComponent<KnightController>().DoDamage;
+            
+            // If first time entering acid
+            if (!this.enteredAcid)
+            {
+                this.enteredAcid = true;
+                DamageKnight(damage);
+                this.nextDamage = Time.time + this.damageInterval;
+            }
+            // If was already in acid and is time to do next damage
+            else if (this.nextDamage <= Time.time)
+            {
+                DamageKnight(damage);
+                this.nextDamage = Time.time + this.damageInterval;
+            }
+        }
+        // Not touching the player
+        else
+        {
+            this.enteredAcid = false;
         }
     }
 }
