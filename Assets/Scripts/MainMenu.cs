3e099afb68f94e3dc381940274c8e12e95e39e3a
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2022-06-04 01:12:04 -0700

    added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void PlayGame()
+   {
+       // buildindex +1 can be edited under "File"-> Build settings and you will see the settings there. 
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+
+   public void QuitGame()
+   {
+       Application.Quit();
+   }
+}
