e7755f197912b829c3ad329234cff1f00c005f72
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2022-06-05 18:57:44 -0700

    added easy,medium,hard scenes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,6 +10,18 @@ public class MainMenu : MonoBehaviour
        // buildindex +1 can be edited under "File"-> Build settings and you will see the settings there. 
        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
+   public void Easy()
+   {
+       SceneManager.LoadScene("Easy");
+   }
+   public void Medium()
+   {
+       SceneManager.LoadScene("Medium");
+   }
+   public void Hard()
+   {
+       SceneManager.LoadScene("Hard");
+   }
 
    public void QuitGame()
    {
